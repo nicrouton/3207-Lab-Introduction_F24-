8a8e11a5ed825dfda25185ddfb9869f6100fa231
Nicholas Croulet <tug61145@temple.edu>
2024-08-28 14:16:57 -0400

    Added preprocessor directives

---
+++ random.c
@@ -0,0 +1,15 @@
+/* Nicholas Croulet
+random.c
+3207 section 701
+
+A function to generate a random character.
+
+*/
+
+// Define a max and minimum for the random number generation based on number of chars
+#define RAND_MIN 0
+#define RAND_MIN 26
+
+char randchar() {
+    // comment
+}
