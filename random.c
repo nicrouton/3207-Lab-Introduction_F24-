a278d4bb99c4a9f3eab6491cba91c0b58dedf805
Nicholas Croulet <tug61145@temple.edu>
2024-08-30 13:03:45 -0400

    Added fixed given code and added for loop with return for randchar. Fixed mistakes with RAND_MAX not existing.

--- random.c
+++ random.c
@@ -8,7 +8,7 @@ A function to generate a random character.
 
 // Define a max and minimum for the random number generation based on number of chars
 #define RAND_MIN 0
-#define RAND_MIN 26
+#define RAND_MAX 26
 
 char randchar() {
     // integer for creating array of alphabet chars
@@ -17,6 +17,10 @@ char randchar() {
 
     // initialize array
     for (int i = 0; i < alphabet; i++) {
-        alphabet = 'A' + i;
+        alphabetLetters[i] = 'A' + i;
     }
+
+    int number = rand() % (RAND_MAX + 1) + RAND_MIN;
+
+    return alphabetLetters[number];
 }
